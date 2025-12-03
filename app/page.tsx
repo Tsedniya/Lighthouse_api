92366f58e150d90d6d666d2614c4d63709b4ffa8
Tsedniya <ktsedniya@gmail.com>
2025-12-03 09:59:29 +0300

    Initial commit: lighthouse-demo

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import LighthouseForm from "../components/LighthouseForm";
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 dark:bg-black">
+      <LighthouseForm />
+    </div>
+  );
+}
